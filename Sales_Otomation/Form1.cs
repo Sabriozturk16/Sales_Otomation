89b07f4f49b22641c1c6a770098355e8da3cc29a
SABRİ ÖZTÜRK <sabriozturk2@posta.mu.edu.tr>
2023-05-10 11:22:33 +0300

    Katmanlı Mimari Oluşturuldu

--- Sales_Otomation/Form1.cs
+++ Sales_Otomation/Form1.cs
@@ -16,5 +16,10 @@ namespace Sales_Otomation
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
